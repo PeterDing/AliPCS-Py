df196eb2315f641e642b2fcc845d361a50fb8416
PeterDing <dfhayst@gmail.com>
2022-08-16 14:19:23 +0800

    Remove debug code

--- alipcs_py/commands/user.py
+++ alipcs_py/commands/user.py
@@ -4,5 +4,4 @@ from alipcs_py.commands.display import display_user_info
 
 def show_user_info(api: AliPCSApi):
     user_info = api.user_info()
-    print(user_info)
     display_user_info(user_info)
