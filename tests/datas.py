593fcb70bacc633d76b8650aed21336e07fd6f68
PeterDing <dfhayst@gmail.com>
2024-04-08 23:02:45 +0800

    Fix type

--- tests/datas.py
+++ tests/datas.py
@@ -1,5 +1,6 @@
 import os
 from dataclasses import dataclass
+from typing import List
 
 from alipcs_py.alipcs.inner import PcsFile
 
@@ -9,7 +10,7 @@ REFRESH_TOKEN = os.getenv("REFRESH_TOKEN", "")
 @dataclass
 class Datas:
     local_dir: str
-    local_paths: list[str]
+    local_paths: List[str]
     remote_dir: str
     remote_dir_pcs_file: PcsFile
-    remote_paths: list[str]
+    remote_paths: List[str]
